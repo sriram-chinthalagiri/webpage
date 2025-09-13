d5f1b6abc118d837c973e93c82eac1fa53f9554c
sriram3301 <sriram.chinthalagiri@iwr.uni-heidelberg.de>
2025-09-13 10:31:38 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -104,7 +104,7 @@ const App = () => {
           <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">Teaching</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-8 max-w-5xl mx-auto">
             {/* Course 1 */}
-            <div className="bg-gray-700 py-2 px-4 rounded-xl shadow-lg transition-transform hover:scale-105 duration-300 ease-in-out">
+            <div className="bg-gray-200 py-2 px-4 rounded-xl shadow-lg transition-transform hover:scale-105 duration-300 ease-in-out">
               <div className={`w-12 h-12 flex items-center justify-center rounded-full ${accentColor} mb-4`}>
                 <BookOpen size={24} className="text-white" />
               </div>
