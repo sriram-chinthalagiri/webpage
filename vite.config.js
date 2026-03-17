367fb080e85402150b1cab05a950ee6960c64e6b
sriram3301 <sriram.chinthalagiri@iwr.uni-heidelberg.de>
2026-03-17 16:39:26 +0100

    new branch changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+<<<<<<< Updated upstream
   plugins: [react()],
   base: './', // <-- Add this line
 });
 
+=======
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  // This is required for GitHub Pages to find your files in the subfolder
+  base: '/webpage/', 
+  build: {
+    outDir: 'docs',
+  }
+})
+>>>>>>> Stashed changes
